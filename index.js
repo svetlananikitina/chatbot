0f765d75c95a029058938d866dbbe4cbdc957284
Svetlana Nikitina <nikitinasvetlana92@gmail.com>
2017-05-27 18:35:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-	res.send('Hello world, I am a chat bot')
+	res.send('Hi world, I am a story bot!')
 })
 
 // for Facebook verification
